a807bf869ca8a97c4dd22a37e54817a7b223a939
Jack Tse <jacktsc@Jacks-MacBook-Pro.local>
2018-06-12 12:06:46 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,11 +16,18 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
-    @group.save
+      @group = Group.new(group_params)
+      @group.save
  
       redirect_to groups_path
   end
+
+  def update
+      @group = Group.find(params[:id])
+      @group.update(group_params)
+
+      redirect_to groups_path, notice: "Update Success"
+  end 
  
   private
  
